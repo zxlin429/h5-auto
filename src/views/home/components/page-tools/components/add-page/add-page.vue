cdd5db42dfee56624f6aa485c77d1c9229e71941
xzeng <xzeng@deloitte.com.cn>
2023-05-04 20:04:24 +0800

    [IMP]修改在没有项目时不展示添加页面的按钮

--- src/views/home/components/page-tools/components/add-page/add-page.vue
+++ src/views/home/components/page-tools/components/add-page/add-page.vue
@@ -2,12 +2,12 @@
  * @Author       : zxlin
  * @Date         : 2023-05-01 02:25:54
  * @LastEditors  : zxlin
- * @LastEditTime : 2023-05-04 14:50:33
- * @FilePath     : \h5-auto\src\views\home\components\page-tools\components\add-page\add-page.vue
+ * @LastEditTime : 2023-05-04 20:04:06
+ * @FilePath     : /h5-auto/src/views/home/components/page-tools/components/add-page/add-page.vue
  * @Description  : add-page
 -->
 <template>
-  <div class="add-page">
+  <div class="add-page" v-if="currentProject">
     <el-button
       type="primary"
       :icon="Plus"
@@ -22,7 +22,7 @@ import { Plus } from '@element-plus/icons-vue';
 import { useStore } from 'vuex';
 import useProject from '@/views/home/hooks/useProject';
 const store = useStore();
-const { currentProjectObject, addPage } = useProject(store);
+const { currentProject, currentProjectObject, addPage } = useProject(store);
 </script>
 <style scoped>
 .add-page {
