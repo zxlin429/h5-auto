52f354c05d0daef386eb8fdacf29ddae4f235fbb
zxlin <zengxinlin@youloft.com>
2023-07-27 09:04:05 +0800

    [IMP]修改vue配置项

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+/*
+ * @Author       : zxlin
+ * @Date         : 2023-04-27 14:39:16
+ * @LastEditors  : zxlin
+ * @LastEditTime : 2023-07-27 09:03:56
+ * @FilePath     : \h5-auto\vue.config.js
+ * @Description  : 
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "./",
+  devServer: {
+    client: {
+      overlay: false
+    }
+  }
 })
